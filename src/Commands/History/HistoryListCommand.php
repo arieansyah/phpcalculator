c5bd82669ed84f76731b7d8f7bc5452c136f0a1e
arieansyah <arieansyahp.bun@gmail.com>
2020-04-20 20:32:25 +0700

    +history command #11

---
+++ src/Commands/History/HistoryListCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Jakmall\Recruitment\Calculator\Commands\History;
+
+use Illuminate\Console\Command;
+use Jakmall\Recruitment\Calculator\Repository\HistoryRepository as History;
+
+class HistoryListCommand extends Command
+{
+    public function __construct()
+    {
+        $this->signature = "history:list {commands?* : Filter the history by commands}";
+        $this->description = "Show Calculator History";
+
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        $this->getHistory($this->argument("commands"));
+        var_dump($this->argument("commands"));
+        return;
+    }
+
+    protected function getHistory(array $input)
+    {
+        $data = History::getAll($input);
+        if ($data) {
+            $this->table(['No', 'Command', 'Description', 'Result', 'Output', 'Time'], $data);
+        } else {
+            $this->comment('History is empty');
+        }
+    }
+}
